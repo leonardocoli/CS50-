49b91eeebccc3755d2d9a739dcdeb57e1fbad343
leonardocoli <79295922+leonardocoli@users.noreply.github.com>
2024-07-16 18:35:37 -0300

    Add files via upload

--- dna.py
+++ dna.py
@@ -1,75 +1,58 @@
 import csv
 import sys
 
-#sequence é o arquivo txt; subsequence é um dos STR
-def longest_match(sequence, subsequence):
-
-  longest_run = 0
-  len_sub = len(subsequence)
-  len_seq = len(sequence)
-
-  for i in range(len_seq):
-    #conta de STR consecutivos
-    count = 0
-
-    while True:
-
-      start = i + count * len_sub
-      end = start + len_sub
-
-      if sequence[start:end] == subsequence:
-        count +=1
-
-      else:
-        break
-    longest_run = max(longest_run, count)
-
-  return longest_run
-
 
 def main():
-    #command line usage
-  if len(sys.argv) != 3:
-      print("Usage: python dna.py data.csv sequence.txt")
-      sys.exit(1)
 
-  #read database into a variablepython dna.py
-  #database é meu arquivo csv em formato de dicionário nome:valor dentro da lista
-  database = []
-  with open(sys.argv[1], 'r') as file:
-      reader = csv.DictReader(file)
-      for row in reader:
-          database.append(row)
-  #print(database)
+    # TODO: Check for command-line usage
 
-  #read sequence file into a variable
-  with open(sys.argv[2], 'r') as file:
-      dna_sequence = file.read()
-  #print(dna_sequence)
+    # TODO: Read database file into a variable
+    
+    # TODO: Read DNA sequence file into a variable
 
-  #find longest match of each STR in DNA sequence
-  subsequences = list(database[0].keys())[1:]
-  #subsequences = AGATC,TTTTTTCT,AATG,TCTAG,GATA,TATC,GAAA,TCTG or AGATC,AATG,TATC
-  #print(len(subsequences))
-  results = {}
-  for i in subsequences:
-      results[i] = longest_match(dna_sequence, i)
-  #print(results)
+    # TODO: Find longest match of each STR in DNA sequence
 
-  #check database to match profiles
-  no_match = 0
-  for i in database:
-      count = 0
-      for j in subsequences:
-          if int(i[j]) == results[j]:
-              count += 1
+    # TODO: Check database for matching profiles
 
-      if count == len(subsequences):
-          print(i["name"])
-          break
+    return
 
-      if count != len(subsequences) and i  == database[-1]:
-          no_match += 1
-          print("no match")
+
+def longest_match(sequence, subsequence):
+    """Returns length of longest run of subsequence in sequence."""
+
+    # Initialize variables
+    longest_run = 0
+    subsequence_length = len(subsequence)
+    sequence_length = len(sequence)
+
+    # Check each character in sequence for most consecutive runs of subsequence
+    for i in range(sequence_length):
+
+        # Initialize count of consecutive runs
+        count = 0
+
+        # Check for a subsequence match in a "substring" (a subset of characters) within sequence
+        # If a match, move substring to next potential match in sequence
+        # Continue moving substring and checking for matches until out of consecutive matches
+        while True:
+
+            # Adjust substring start and end
+            start = i + count * subsequence_length
+            end = start + subsequence_length
+
+            # If there is a match in the substring
+            if sequence[start:end] == subsequence:
+                count += 1
+            
+            # If there is no match in the substring
+            else:
+                break
+        
+        # Update most consecutive matches found
+        longest_run = max(longest_run, count)
+
+    # After checking for runs at each character in seqeuence, return longest run found
+    return longest_run
+
 
 main()
